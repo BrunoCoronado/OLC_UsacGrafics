85ce1f15a2a98d6c3af5eb63026752634387377d
BrunoCoronado <bcoronado.morales@gmail.com>
2019-02-08 21:04:08 -0600

    Creación Proyecto
    Creación Proyecto

---
+++ src/main/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import sistema.ui.ventanaPrincipal;
+
+/**
+ *
+ * @author bruno
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ventanaPrincipal ventanaPrincipal = new ventanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+    }
+    
+}
